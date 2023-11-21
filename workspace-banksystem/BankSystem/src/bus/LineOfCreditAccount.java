68add3ca6a1bbc19c262290a8a5957755f6e8d8b
bonnieives <bonnieives@gmail.com>
2023-11-21 13:31:37 -0500

    adding checking, lineofcredit and creditaccount

---
+++ workspace-banksystem/BankSystem/src/bus/LineOfCreditAccount.java
@@ -0,0 +1,38 @@
+package bus;
+
+public class LineOfCreditAccount extends Account {
+	
+	private Double currentBalance;
+	private Double lateFee;
+	
+	public LineOfCreditAccount() {
+		super();
+	}
+	
+	public LineOfCreditAccount(Double currentBalance, Double lateFee) {
+		super();
+		this.currentBalance = currentBalance;
+		this.lateFee = lateFee;
+	}
+	
+	public double getCurrentBalance() {
+		return this.currentBalance;
+	}
+	
+	public void setCurrentBalance(Double currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+	
+	public double getLateFee() {
+		return this.lateFee;
+	}
+	
+	public void setLateFee(Double lateFee) {
+		this.lateFee = lateFee;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "LineOfCreditAccount [currentBalance="+currentBalance+", lateFee="+lateFee+"]";
+	}
+}
